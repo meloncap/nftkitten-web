cf70ee8d73356a98aaf91bff3e71c5708b26e9b2
nftkitten <nftkitten+github@pm.me>
2022-03-26 22:43:50 +0800

    use css module for loadingscreen

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,19 +1,52 @@
+import styles from '../styles/loadingscreen.module.css'
+
 export const LoadingScreen = () => (
-  <div className='loading-screen'>
-    <p>LOADING...</p>
-    <i>
-      <b>N</b>
-      <b>F</b>
-      <b>T</b>
-      <b>K</b>
-      <b>i</b>
-      <b>t</b>
-      <b>t</b>
-      <b>e</b>
-      <b>n</b>
-      <b>.</b>
-      <b>i</b>
-      <b>o</b>
-    </i>
+  <div
+    className={
+      'flex fixed inset-0 justify-center items-center w-screen h-screen no-underline pointer-events-none ' +
+      styles['loading-screen']
+    }
+  >
+    <p className='font-sans text-xs text-gray-500 dark:text-gray-200'>
+      LOADING...
+    </p>
+    <div className='cursor-default pointer-events-none select-none'>
+      <b className='absolute font-sans text-xl text-gray-700 dark:text-white opacity-0'>
+        N
+      </b>
+      <b className='absolute font-sans text-xl text-gray-700 dark:text-white opacity-0'>
+        F
+      </b>
+      <b className='absolute font-sans text-xl text-gray-700 dark:text-white opacity-0'>
+        T
+      </b>
+      <b className='absolute font-sans text-xl text-blue-500 dark:text-blue-300 opacity-0'>
+        K
+      </b>
+      <b className='absolute font-sans text-xl text-blue-500 dark:text-blue-300 opacity-0'>
+        i
+      </b>
+      <b className='absolute font-sans text-xl text-blue-500 dark:text-blue-300 opacity-0'>
+        t
+      </b>
+      <b className='absolute font-sans text-xl text-blue-500 dark:text-blue-300 opacity-0'>
+        t
+      </b>
+      <b className='absolute font-sans text-xl text-blue-500 dark:text-blue-300 opacity-0'>
+        e
+      </b>
+      <b className='absolute font-sans text-xl text-blue-500 dark:text-blue-300 opacity-0'>
+        n
+      </b>
+      <b className='absolute font-sans text-xl text-gray-700 dark:text-white opacity-0'>
+        .
+      </b>
+      <b className='absolute font-sans text-xl text-gray-700 dark:text-white opacity-0'>
+        i
+      </b>
+      <b className='absolute font-sans text-xl text-gray-700 dark:text-white opacity-0'>
+        o
+      </b>
+    </div>
   </div>
 )
