06eeef10c87312b776ada441cc140f1331b78672
nftkitten <nftkitten+github@pm.me>
2022-03-29 14:38:00 +0800

    handle ipfs://

--- src/services/mediaUrlLoader.ts
+++ src/services/mediaUrlLoader.ts
@@ -2,6 +2,9 @@ import { useMyStore } from '../hooks/useMyStore'
 
 export function mediaUrlLoader(src: string, width: number, height: number) {
   const imageBaseUrl = useMyStore.getState().imageBaseUrl
+  if (/^ipfs:\/\//.test(src)) {
+    src = 'https://ipfs.io/ipfs/' +  src.replace(/^ipfs:\/\//, '')
+  }
   const f = /\.svg($|\?)/i.test(src) ? 'f_auto' : 'f_webp'
   const q = width <= 100 && height <= 100 ? 'q_auto:low' : 'q_auto'
   let url = [
